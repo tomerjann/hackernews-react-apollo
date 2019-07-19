e2b77ba103f988ea662b8dd2f44293f0a58d411e
tomerjann <tomer.jann@gmail.com>
2019-07-19 20:05:29 +0300

    all files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 1
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { BrowserRouter } from "react-router-dom";
+import { setContext } from "apollo-link-context";
+import { AUTH_TOKEN } from "./constants";
+import { split } from "apollo-link";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// 2
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000"
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN)
+    }
+  }
+});
+
+// split is used to “route” a request to a specific middleware link.
+//It takes three arguments, the first one is a test function which returns a boolean.
+//The remaining two arguments are again of type ApolloLink.
+//If test returns true, the request will be forwarded to the link passed as the second argument.
+//If false, to the third one
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
+// 3
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
+// 4
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
